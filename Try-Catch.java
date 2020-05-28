53128ac6ce2bdb8ff06ee70a53e2c7079ce20187
mikenwoo <63419730+mikenwoo@users.noreply.github.com>
2020-05-29 03:27:53 +0900

    Create Try-Catch.java

---
+++ Try-Catch.java
@@ -0,0 +1,10 @@
+public class Try-Catch {
+    try {
+        //function you would like to test out
+        //or has the potential to get error
+    } catch (Exception e) {
+        //When the try statement catches a certain kind of exception
+        //You would write what you want to do after having that error
+        //in this catch method
+    }
+}
